c3ecfd084cd3e3244b531548846366cab4027c86
BathwithRadio <thigma1201@gmail.com>
2026-04-14 19:29:34 +0800

    My First MCP server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp';
+import z, { symbol } from 'zod';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		const server = new McpServer({
+			name: 'Sticks Server',
+			version: '1.0',
+		});
+
+		server.registerTool(
+			'get-stock-price',
+			{
+				//
+				description: 'Get the price of a stock given a ticker symbol.',
+				// AI모델한테 이 tool에 argument가 필요하다고 알려주기 위해 사용
+				inputSchema: {
+					symbol: z.string(),
+				},
+			},
+			async ({ symbol }) => {
+				return {
+					content: [
+						{
+							type: 'text',
+							text: 'the price of ${symbol} is $10 USD.',
+						},
+					],
+				};
+			},
+		);
+
+		return new Response('ByeBye World!');
 	},
 } satisfies ExportedHandler<Env>;
+
+// cb : 실제로 tool이 호출될 때 우리 서버에서 실행
