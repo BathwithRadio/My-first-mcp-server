7ff91377fc761fd4ae92679c641a4a2b1a0e6d94
BathwithRadio <thigma1201@gmail.com>
2026-04-15 20:32:04 +0800

    first resource

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import z from 'zod';
 import { createMcpHandler } from 'agents/mcp';
+import { registerAppResource, registerAppTool, RESOURCE_MIME_TYPE } from '@modelcontextprotocol/ext-apps/server';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
@@ -9,7 +10,20 @@ export default {
 			version: '1.0',
 		});
 
-		server.registerTool(
+		registerAppResource(server, 'Stocks Widget', 'ui://stocks-ui', { description: 'UI of the stocks tool' }, async () => {
+			return {
+				contents: [
+					{
+						uri: 'ui://stocks-ui',
+						text: '<html><body><h1>Hello world</h1></body></html>',
+						mimeType: RESOURCE_MIME_TYPE,
+					},
+				],
+			};
+		});
+
+		registerAppTool(
+			server,
 			'get-stock-price',
 			{
 				//
@@ -17,6 +31,12 @@ export default {
 				// AI모델한테 이 tool에 argument가 필요하다고 알려주기 위해 사용
 				inputSchema: {
 					symbol: z.string(),
+				},
+				// registerAppTool로 텍스트 뿐만이 아니라 아래의 ui도 같이 보여달라고 말하게 되는 것
+				_meta: {
+					ui: {
+						resourceUri: 'ui://stocks-ui',
+					},
 				},
 			},
 			async ({ symbol }) => {
